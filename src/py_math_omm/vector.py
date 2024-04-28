c1721301d5f304b0a1769944a0d1a039c4dab1f1
Omer M <omerplays1411@gmail.com>
2024-04-28 16:29:53 +0300

    added some vector methods

--- src/py_math_omm/vector.py
+++ src/py_math_omm/vector.py
@@ -6,7 +6,10 @@ S = TypeVar('S')
 
 class VectorElement(Protocol):
     def __mul__(self, other: S, /) -> S: ...
+    def __truediv__(self, other: S, /) -> S: ...
     def __add__(self, other: S, /) -> S: ...
+    def __sub__(self, other: S, /) -> S: ...
+    def __neg__(self) -> S: ...
     def __eq__(self, other: object, /) -> bool: ...
     def __str__(self) -> str: ...
     def __repr__(self) -> str: ...
@@ -53,19 +56,48 @@ class Vector(Generic[T]):
 
         return res
 
-    def __add__(self, other: "Vector[T]") -> "Vector[T]":
-        values: list[T] = []
+    def __add__(self, other: "Vector[T]", /) -> "Vector[T]":
         length: int = self.length
         other_length: int = other.length
         if length < other_length:
             return other.__add__(self)
-        else:
-            for i in range(length):
-                if i < other_length:
-                    values.append(self.values[i] + other.values[i])
-                else:
-                    values.append(self.values[i])
-        return Vector(*values)
+
+        new_values_generator = (
+            (self.values[i] + other.values[i] if i < other_length else self.values[i]) for i in range(length))
+
+        return Vector.from_iterable(new_values_generator)
+
+    def __sub__(self, other: "Vector[T]", /) -> "Vector[T]":
+        length: int = self.length
+        other_length: int = other.length
+        if length < other_length:
+            new_values_generator = (
+                (self.values[i] - other.values[i] if i < length else -other.values[i]) for i in range(other_length))
+
+            return Vector.from_iterable(new_values_generator)
+
+        new_values_generator = (
+            (self.values[i] - other.values[i] if i < other_length else self.values[i]) for i in range(length))
+
+        return Vector.from_iterable(new_values_generator)
+
+    def __mul__(self, other: T, /) -> "Vector[T]":
+        return Vector.from_iterable((i * other for i in self.values))
+
+    def __rmul__(self, other: T, /) -> "Vector[T]":
+        return self * other
+
+    def __truediv__(self, other: T, /) -> "Vector[T]":
+        return Vector.from_iterable((i / other for i in self.values))
+
+    def __len__(self) -> int:
+        return self.length
+
+    def __neg__(self) -> "Vector[T]":
+        return Vector.from_iterable((-i for i in self.values))
+
+    def __pos__(self) -> "Vector[T]":
+        return Vector.from_iterable(self.values)
 
     @overload
     def __getitem__(self, i: SupportsIndex, /) -> T: ...
@@ -73,13 +105,13 @@ class Vector(Generic[T]):
     @overload
     def __getitem__(self, s: slice, /) -> list[T]: ...
 
-    def __getitem__(self, i: SupportsIndex | slice) -> T | list[T]:
+    def __getitem__(self, i: SupportsIndex | slice, /) -> T | list[T]:
         return self.values[i]
 
     def __iter__(self):
         return self.values
 
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: object, /) -> bool:
         if not isinstance(other, Vector):
             return False
 
@@ -92,7 +124,7 @@ class Vector(Generic[T]):
 
         return True
 
-    def __ne__(self, value: object) -> bool:
+    def __ne__(self, value: object, /) -> bool:
         return not self == value
 
     def __repr__(self) -> str:
